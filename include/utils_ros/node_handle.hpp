c4dabc6626c427e8d65bbddf69ce46c06fa13b4b
Andre-Marcel Hellmund <andre-marcel.hellmund@kit.edu>
2016-07-28 11:26:58 +0200

    added getParam functions with return value

--- include/utils_ros/node_handle.hpp
+++ include/utils_ros/node_handle.hpp
@@ -1,20 +1,41 @@
 #pragma once
 
-#include <ros/node_handle.h>
 #include <stdlib.h>
+#include <ros/node_handle.h>
 
 namespace utils_ros {
 
-template<typename T>
-inline void getParam(const ros::NodeHandle& nodeHandle, const std::string key, T& val) {
-	if (!nodeHandle.getParam(key, val)) {
-		ROS_ERROR_STREAM("Parameter '" << key << "' is not defined.");
-		std::exit(EXIT_FAILURE);
-	}
+template <typename T>
+inline void getParam(const ros::NodeHandle& nodeHandle, const std::string& key, T& val) {
+    if (!nodeHandle.getParam(key, val)) {
+        ROS_ERROR_STREAM("Parameter '" << key << "' is not defined.");
+        std::exit(EXIT_FAILURE);
+    }
 }
 
-template<typename T>
-inline void getParam(const ros::NodeHandle& nodeHandle, const std::string key, T& val, const T& defaultValue) {
+template <typename T>
+inline void getParam(const ros::NodeHandle& nodeHandle, const std::string& key, T& val, const T& defaultValue) {
     nodeHandle.param(key, val, defaultValue);
+}
+
+template <typename T>
+inline T getParam(const ros::NodeHandle& nodeHandle, const std::string& key) {
+    if (!nodeHandle.hasParam(key)) {
+        ROS_ERROR_STREAM("Parameter '" << key << "' is not defined.");
+        std::exit(EXIT_FAILURE);
+    }
+    T val;
+    nodeHandle.getParam(key, val);
+    return val;
+}
+
+template <typename T>
+inline T getParam(const ros::NodeHandle& nodeHandle, const std::string& key, const T& defaultValue) {
+    if (!nodeHandle.hasParam(key)) {
+        return defaultValue;
+    }
+    T val;
+    nodeHandle.getParam(key, val);
+    return val;
 }
 }
